f5a12e03bf6ebaf331f894c86e43e6015464423e
Franco Silva <franco_tigres18@hotmail.com>
2023-03-11 17:55:47 -0600

    Header add

---
+++ views/templates/header.php
@@ -0,0 +1,14 @@
+<header class="header">
+  <div class="header__contenedor header__contenido">
+    <h1 class="header__titulo">Estetica Unisex Silvia</h1>
+    <nav class="header__navegacion">
+      <a href="#" class="header__enlace">Iniciar Sesión</a>
+      <a href="#" class="header__enlace">Registrarse</a>
+    </nav>
+  </div>
+
+  <div class="header__texto">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ut, praesentium consequatur ullam sapiente eum?</p>
+  </div>
+
+</header>
