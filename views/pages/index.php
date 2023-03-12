1231e0f7b4c1321dff18f5a239e1509a591979bd
Franco Silva <franco_tigres18@hotmail.com>
2023-03-12 17:08:58 -0600

    Estilos servicios add

--- views/pages/index.php
+++ views/pages/index.php
@@ -1,1 +1,38 @@
-<h1>Estética Silvia</h1>
+<main class="servicios">
+  <h2 class="servicios__heading">Servicios</h2>
+  <div class="servicios__grid">
+    <div class="servicio">
+      <h3 class="servicio__titulo">Corte de Cabello Hombre</h3>
+      <p class="servicio__precio">$50 - $100</p>
+      <div class="servicio__iconos">
+        <i class="fa-solid fa-scissors"></i>
+        <i class="fa-solid fa-couch"></i>
+      </div>
+    </div>
+    <div class="servicio">
+      <h3 class="servicio__titulo">Corte de Cabello Hombre</h3>
+      <p class="servicio__precio">$50 - $100</p>
+      <div class="servicio__iconos">
+        <i class="fa-solid fa-scissors"></i>
+        <i class="fa-solid fa-couch"></i>
+      </div>
+    </div>
+    <div class="servicio">
+      <h3 class="servicio__titulo">Corte de Cabello Hombre</h3>
+      <p class="servicio__precio">$50 - $100</p>
+      <div class="servicio__iconos">
+        <i class="fa-solid fa-scissors"></i>
+        <i class="fa-solid fa-couch"></i>
+      </div>
+    </div>
+    <div class="servicio">
+      <h3 class="servicio__titulo">Corte de Cabello Hombre</h3>
+      <p class="servicio__precio">$50 - $100</p>
+      <div class="servicio__iconos">
+        <i class="fa-solid fa-scissors"></i>
+        <i class="fa-solid fa-couch"></i>
+      </div>
+    </div>
+  </div>
+
+</main>
