473c44cc59d5076cac44eb67bbc84ff83d80e233
Franco Silva <franco_tigres18@hotmail.com>
2023-04-01 20:26:46 -0600

    Header Fixed

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,14 +1,17 @@
 <header class="header">
-  <div class="header__contenedor header__contenido">
-    <h1 class="header__titulo">Estetica Unisex Silvia</h1>
-    <nav class="header__navegacion">
-      <a href="#" class="header__enlace">Iniciar Sesión</a>
-      <a href="#" class="header__enlace">Registrarse</a>
-    </nav>
-  </div>
+  <div class="header__contenedor">
 
-  <div class="header__texto">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ut, praesentium consequatur ullam sapiente eum?</p>
+    <div class="header__barra">
+      <h2 class="header__titulo">Estetica Unisex Silvia</h2>
+      <nav class="navegacion">
+        <a href="#" class="navegacion__enlace">Iniciar Sesión</a>
+        <a href="#" class="navegacion__enlace">Registrarse</a>
+      </nav>
+    </div>
+
+    <div class="header__texto">
+      <h1>Agenda tu cita, asi no tendrás que esperar</h1>
+    </div>
   </div>
 
 </header>
